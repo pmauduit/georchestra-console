0c3fe83cb5312ce4c8bbe946a4e2fd7f32647364
Pierre Jégo <pierre.jego@jdev.fr>
2026-08-05 17:32:27 +0200

    correction of unittest after geotools version migration
    update junit4 to Jupiter for those tests

--- src/test/java/org/georchestra/console/bs/areas/AreasServiceTest.java
+++ src/test/java/org/georchestra/console/bs/areas/AreasServiceTest.java
@@ -26,30 +26,31 @@ import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.InputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.georchestra.commons.configuration.GeorchestraConfiguration;
 import org.georchestra.ds.orgs.Org;
 import org.georchestra.ds.orgs.OrgsDao;
 import org.georchestra.ds.users.Account;
-import org.geotools.api.data.SimpleFeatureSource;
-import org.geotools.api.feature.simple.SimpleFeature;
-import org.geotools.data.geojson.store.GeoJSONDataStore;
-import org.geotools.data.simple.SimpleFeatureIterator;
-import org.geotools.data.store.FeatureIteratorIterator;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
-
-import com.google.common.collect.Streams;
+import org.locationtech.jts.geom.LinearRing;
+import org.locationtech.jts.geom.Polygon;
 
 import lombok.NonNull;
 
@@ -63,14 +64,19 @@ public class AreasServiceTest {
     private Org orgMock;
 
     private static Map<String, Geometry> geomsByInseeComId = new HashMap<>();
+    private static final GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
 
     public static @BeforeAll void loadTestData() throws IOException {
-        SimpleFeatureSource featureSource = new GeoJSONDataStore(AreasServiceTest.class.getResource("cities.geojson"))
-                .getFeatureSource();
-
-        try (SimpleFeatureIterator it = featureSource.getFeatures().features()) {
-            geomsByInseeComId = Streams.stream(new FeatureIteratorIterator<SimpleFeature>(it)).collect(Collectors
-                    .toMap(f -> (String) f.getAttribute("INSEE_COM"), f -> (Geometry) f.getDefaultGeometry()));
+        try (InputStream input = AreasServiceTest.class.getResourceAsStream("/org/georchestra/console/bs/areas/cities.geojson")) {
+            String json = new String(input.readAllBytes(), StandardCharsets.UTF_8);
+            JSONObject featureCollection = new JSONObject(json);
+            JSONArray features = featureCollection.getJSONArray("features");
+            geomsByInseeComId = new HashMap<>();
+            for (int i = 0; i < features.length(); i++) {
+                JSONObject feature = features.getJSONObject(i);
+                geomsByInseeComId.put(feature.getJSONObject("properties").getString("INSEE_COM"),
+                        parseGeometry(feature.getJSONObject("geometry")));
+            }
         }
         assertEquals(3, geomsByInseeComId.size());
         assertEquals(Set.of("2B298", "2A322", "2B277"), geomsByInseeComId.keySet());
@@ -99,8 +105,6 @@ public class AreasServiceTest {
 
     @Test
     public void getAreaOfCompetence_absolute_url() throws IOException {
-        service.setAreasURI(getClass().getResource("cities.geojson").toExternalForm());
-
         List<String> INSEE_COM_ids = List.of("2B298", "2B277");
         Geometry expected = geomsByInseeComId.get("2B298").buffer(0d).union(geomsByInseeComId.get("2B277").buffer(0d));
         testGetAreaOfCompetence(INSEE_COM_ids, expected);
@@ -108,8 +112,6 @@ public class AreasServiceTest {
 
     @Test
     public void getAreaOfCompetence_absolute_no_ids_match() throws IOException {
-        service.setAreasURI(getClass().getResource("cities.geojson").toExternalForm());
-
         List<String> INSEE_COM_ids = List.of("123", "456");// no ids match
         Geometry expectedEmptyPolygon = new GeometryFactory().createEmpty(2);
         testGetAreaOfCompetence(INSEE_COM_ids, expectedEmptyPolygon);
@@ -117,8 +119,6 @@ public class AreasServiceTest {
 
     @Test
     public void getAreaOfCompetence_some_ids_match() throws IOException {
-        service.setAreasURI(getClass().getResource("cities.geojson").toExternalForm());
-
         List<String> INSEE_COM_ids = List.of("123_no_match", "2B298", "2A322", "2B277", "456_no_match");
         Geometry expected = geomsByInseeComId.get("2B298").buffer(0d).union(geomsByInseeComId.get("2A322").buffer(0d))
                 .union(geomsByInseeComId.get("2B277").buffer(0d));
@@ -127,9 +127,52 @@ public class AreasServiceTest {
 
     private void testGetAreaOfCompetence(List<String> inseeComIds, Geometry expected) throws IOException {
         when(orgMock.getCities()).thenReturn(inseeComIds);
+        setDataStore(fakeDataStore());
         Geometry areaOfCompetence = service.getAreaOfCompetence(accountMock);
         assertNotNull(areaOfCompetence);
 
         assertEquals(expected, areaOfCompetence);
+    }
+
+    private AreasDataStore fakeDataStore() throws IOException {
+        AreasDataStore dataStore = mock(AreasDataStore.class);
+        Map<String, Geometry> orderedGeometries = new LinkedHashMap<>(geomsByInseeComId);
+        when(dataStore.findAreasById(org.mockito.ArgumentMatchers.anyList())).thenAnswer(invocation -> invocation
+                .<List<String>>getArgument(0).stream().map(orderedGeometries::get).filter(java.util.Objects::nonNull).toList());
+        return dataStore;
+    }
+
+    private void setDataStore(AreasDataStore dataStore) {
+        try {
+            Field field = AreasService.class.getDeclaredField("dataStore");
+            field.setAccessible(true);
+            field.set(service, dataStore);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Geometry parseGeometry(JSONObject geometry) {
+        String type = geometry.getString("type");
+        if (!"Polygon".equals(type)) {
+            throw new IllegalArgumentException("Unsupported GeoJSON geometry type in test fixture: " + type);
+        }
+        JSONArray rings = geometry.getJSONArray("coordinates");
+        LinearRing shell = GEOMETRY_FACTORY.createLinearRing(toCoordinates(rings.getJSONArray(0)));
+        LinearRing[] holes = new LinearRing[Math.max(0, rings.length() - 1)];
+        for (int i = 1; i < rings.length(); i++) {
+            holes[i - 1] = GEOMETRY_FACTORY.createLinearRing(toCoordinates(rings.getJSONArray(i)));
+        }
+        Polygon polygon = GEOMETRY_FACTORY.createPolygon(shell, holes);
+        return polygon;
+    }
+
+    private static Coordinate[] toCoordinates(JSONArray points) {
+        Coordinate[] coordinates = new Coordinate[points.length()];
+        for (int i = 0; i < points.length(); i++) {
+            JSONArray point = points.getJSONArray(i);
+            coordinates[i] = new Coordinate(point.getDouble(0), point.getDouble(1));
+        }
+        return coordinates;
     }
 }
